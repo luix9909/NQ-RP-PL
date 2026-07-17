b414affd965c8a60542da85f194c39ba8abc9f5b
luix9909 <114885726+luix9909@users.noreply.github.com>
2026-07-17 04:35:10 +0300

    Add Supabase connection settings in config.js
    Add Supabase connection settings and client initialization.

---
+++ config.js
@@ -0,0 +1,6 @@
+// إعدادات الاتصال بـ Supabase
+const SUPABASE_URL = "https://oskelvbndtqvaxxfujhs.supabase.co";
+const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9za2VsdmJuZHRxdmF4eGZ1amhzIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODQyMjA5MDYsImV4cCI6MjA5OTc5NjkwNn0.qI_Khh_p1gNFHuwTZPuedqC6WmiWf2IzHWqchGL1yf4";
+
+// تهيئة العميل للاتصال تلقائياً
+const supabase = window.supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
